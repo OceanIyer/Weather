4b0cb242a20717c6a39213930a60c7a81f919547
Ocean <oceaniyer9@gmailcom>
2020-04-23 11:31:13 +0200

    Updates

---
+++ precache-manifest.4b242a7c65ca3730e77b2a56cdfc3d02.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e22d53876f1fd4a0619197efff5590e",
+    "url": "/Weather/index.html"
+  },
+  {
+    "revision": "2dd49621fe37812e93c6",
+    "url": "/Weather/static/css/main.fdd4a8c8.chunk.css"
+  },
+  {
+    "revision": "8ddbb7d98f5b77cd757f",
+    "url": "/Weather/static/js/2.336ea6e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Weather/static/js/2.336ea6e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2dd49621fe37812e93c6",
+    "url": "/Weather/static/js/main.ed1910e0.chunk.js"
+  },
+  {
+    "revision": "49ae7d977cf1b96c820f",
+    "url": "/Weather/static/js/runtime-main.44e11c11.js"
+  },
+  {
+    "revision": "c708fbbe73760b8ccf7ca8e82520f500",
+    "url": "/Weather/static/media/iPhone X-XS-11 Pro – 1.c708fbbe.png"
+  },
+  {
+    "revision": "ae1d923e63462af8157126a3a5b82df8",
+    "url": "/Weather/static/media/iPhone X-XS-11 Pro – 2.ae1d923e.png"
+  }
+]);
